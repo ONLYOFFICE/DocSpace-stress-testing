9fbcab35c1630edc5b85863ab82f63d233e25b44
diana-vahomskaya <diana.vahomskaya@mail.ru>
2024-03-14 14:32:19 +0300

    fix

--- config/install/grafana-influxdb-install.bat
+++ config/install/grafana-influxdb-install.bat
@@ -1,5 +1,4 @@
 choco install k6
 cd "../docker/grafana-influxdb"
 docker-compose up -d
-docker exec -it influxdb influx
 pause
--- config/install/kibana-elasticseach-install.bat
+++ config/install/kibana-elasticseach-install.bat
@@ -1,3 +1,4 @@
+choco install k6
 cd "../.."
 go install go.k6.io/xk6/cmd/xk6@latest
 xk6 build --with github.com/elastic/xk6-output-elasticsearch
